7bc56d9ce17d98180a3169aee9ca65c6ff0c4e29
lujing <13026118978@163.com>
2020-12-17 16:39:16 +0800

    初始化============

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: lujing
  * @Date: 2020-12-17 14:54:44
  * @LastEditors: lujing
- * @LastEditTime: 2020-12-17 16:31:29
+ * @LastEditTime: 2020-12-17 16:38:18
  * @Descripttion: 
  */
 let aa = 0;
@@ -10,3 +10,6 @@ let bb;
 window
 
 //从这里开始
+
+
+// 下面是主分支开发的功能
