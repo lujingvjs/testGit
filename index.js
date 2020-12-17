51bacab5c8ef009df5c33114f63ef8b8aa78f5fc
lujing <13026118978@163.com>
2020-12-17 15:07:58 +0800

    test提交2

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @Author: lujing
  * @Date: 2020-12-17 14:54:44
  * @LastEditors: lujing
- * @LastEditTime: 2020-12-17 15:00:51
+ * @LastEditTime: 2020-12-17 15:06:41
  * @Descripttion: 
  */
 let aa = 0;
 let bb;
+window
