9e537036f965c6134089e9b101b98e617a6232ec
lujing <13026118978@163.com>
2020-12-17 16:41:48 +0800

    devLujing分支开发了计算器功能

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: lujing
  * @Date: 2020-12-17 14:54:44
  * @LastEditors: lujing
- * @LastEditTime: 2020-12-17 16:38:18
+ * @LastEditTime: 2020-12-17 16:41:25
  * @Descripttion: 
  */
 let aa = 0;
@@ -11,5 +11,7 @@ window
 
 //从这里开始
 
-
-// 下面是主分支开发的功能
+// dev分支开发的计算器功能
+function compute(a,b){
+  return a+b;
+}
